c06a4f0832de78fb7e0fd34165f2ae5f08a9a035
AshmiGurjar <ashmi.dgurjar@gmail.com>
2025-03-24 21:30:39 +0530

    24-03-2025
    Decrement

--- src/ex_06_IncrementDecrement_Operator/Lab026_PrePost_Decrement.java
+++ src/ex_06_IncrementDecrement_Operator/Lab026_PrePost_Decrement.java
@@ -2,6 +2,20 @@ package ex_06_IncrementDecrement_Operator;
 
 public class Lab026_PrePost_Decrement {
     public static void main(String[] args) {
+        //Pre-Decrement -> --Operand
+        //Value is Decremented first and then store into the result
+        //Decreased by 1
 
+        int a =10;
+        int b =--a;
+        System.out.println(b);
+        System.out.println(a);
+
+        //Post-Increment -> Operand--
+        //Value is print first and then Decremented first
+        //Decreased by 1
+        int a_post=10;
+        System.out.println("Post Decrement value: "+a_post--);
+        System.out.println(a_post);
     }
 }
