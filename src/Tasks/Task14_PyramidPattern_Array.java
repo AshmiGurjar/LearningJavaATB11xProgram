b6372043083e37c73fda9da113027669054d8ff8
AshmiGurjar <ashmi.dgurjar@gmail.com>
2025-03-31 08:16:05 +0530

    > Tasks

--- src/Tasks/Task14_PyramidPattern_Array.java
+++ src/Tasks/Task14_PyramidPattern_Array.java
@@ -1,6 +1,6 @@
 package Tasks;
 
-public class Task13_PyramidPattern_Array {
+public class Task14_PyramidPattern_Array {
     public static void main(String[] args) {
 //       *
 //      ***
---
+++ src/Tasks/Task15_ClsPersonWMethod.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class Task15_ClsPersonWMethod {
+    public static void main(String[] args) {
+        //Create a Class of a Person with A - 10, B - all 4 types of methods
+        // 1.Without Parameters and Without Return Type
+        // 2.Without Parameters but With Return Type
+        // 3.With Parameters and Without Return Type
+        // 4. With Parameters and With Return Type
+    }
+}
